6e40abeca21cfa34c50d955a5d1cc2d34ae7f5e3
dinellipaulo <pgsd1453@gmail.com>
2026-01-07 13:40:27 -0400

    Logando usuários no sistema

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -17,9 +17,34 @@ exports.register = async (req, res) => {
         }
 
         req.flash('success', 'Usuário criado com sucesso.');
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
             req.session.save(function () {
                 return res.redirect('/login/index');
             });
+            return;
+        }
+
+        req.flash('success', 'Usuário logado com sucesso.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
 
     } catch (e) {
         console.log(e);
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -4,7 +4,7 @@ const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
-    password: { type: String, required: true}
+    password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -16,9 +16,9 @@ class Login {
         this.user = null;
     }
 
-    cleanUp(){
-        for(const key in this.body) {
-            if(typeof this.body[key] !== 'string'){
+    cleanUp() {
+        for (const key in this.body) {
+            if (typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
@@ -29,29 +29,46 @@ class Login {
         }
     }
 
-    valida(){
+    valida() {
         this.cleanUp();
 
         // Validações
 
         // O e-mail precisa ser válido
-        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+        if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
         // A senha precisa ter entre 6 e 50 caracteres
-        if(this.body.password.length < 3 || this.body.password.length > 50) 
+        if (this.body.password.length < 3 || this.body.password.length > 50)
             this.errors.push('A senha precisa ter entre 3 a 50 caracteres');
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({ email: this.body.email });
-        if(user) this.errors.push('Este usuário já existe.');
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Este usuário já existe.');
     }
 
-    async register(){
+    async login() {
+        this.valida()
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if (!this.user) {
+            this.errors.push('Email ou senha inválidos.');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Email ou senha inválidos.');
+            this.user = null;
+            return;
+        };
+    }
+
+    async register() {
         this.valida();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
         await this.userExists();
-        if(this.errors.length > 0) return;
+        if (this.errors.length > 0) return;
 
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
